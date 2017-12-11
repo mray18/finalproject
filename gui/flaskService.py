de447dca602595199b9ffc40fdec242154703966
mray18 <mray218@vt.edu>
2017-12-11 00:24:53 +0000

    working ui interface

---
+++ gui/flaskService.py
@@ -0,0 +1,17 @@
+#/flask/bin/python3
+from flask import *
+import netifaces
+
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+   return render_template('testPage.html')
+
+ips = netifaces.ifaddresses('wlan0')
+tempList = ips[2]
+var = tempList[0]
+address = var['addr']
+
+
+app.run(host=address, port=9000, debug=False)
