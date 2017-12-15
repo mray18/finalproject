cf8c1b343adfcf9592088c72829af08c083bd546
mray18 <mray218@vt.edu>
2017-12-15 21:08:00 +0000

    getting rid of hardcode ip

--- gui/flaskService.py
+++ gui/flaskService.py
@@ -2,21 +2,23 @@
 from flask import *
 import netifaces
 
+ips = netifaces.ifaddresses('wlan0')
+tempList = ips[2]
+var = tempList[0]
+address = var['addr']
 
 app = Flask(__name__)
 @app.route('/')
-def home():
-   return render_template('index.html')
+def home(iaddress=address):
+    return render_template('index.html', iaddress=iaddress)
+
 @app.route('/createEvent')
 def create():
    return render_template('newEvent.html')
 @app.route('/updateEvent')
 def update():
    return render_template('updateEvent.html')
-ips = netifaces.ifaddresses('wlan0')
-tempList = ips[2]
-var = tempList[0]
-address = var['addr']
+
 
 
 app.run(host=address, port=9000, debug=False)
