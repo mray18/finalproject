c3a39278eba7ae69eb538094d4513a12d6e06d79
smp27768 <32310695+smp27768@users.noreply.github.com>
2017-12-17 17:27:52 -0500

    Modified back_end.py

--- back_end.py
+++ back_end.py
@@ -8,6 +8,59 @@ from pymongo import MongoClient
 from datetime import datetime, timedelta
 app = Flask(__name__)
 
+#--------------------For Google Calendar---------------------
+import httplib2
+import os
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+import datetime
+#------------------------------------------------------------
+
+#---------------Possible Zero-Configuration------------------
+import sys
+import time
+import logging
+import socket
+from zeroconf import ServiceInfo, Zeroconf
+
+#Get the IP address of the back-end Raspberry Pi
+def get_ip_address():
+    ip_address = '';
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    ip_address = s.getsockname()[0]
+    s.close()
+    return ip_address
+	
+try:
+    # From this link: https://github.com/jstasiak/python-zeroconf/blob/master/examples/registration.py
+    logging.basicConfig(level=logging.DEBUG)
+    if len(sys.argv) > 1:
+        assert sys.argv[1:] == ['--debug']
+        logging.getLogger('zeroconf').setLevel(logging.DEBUG)
+        
+    desc = {'Description': 'Zero-configuration for Team 14-Final Project in ECE 4564.'}
+
+    #Get the local IP address of the Pi running led.py
+    localip = socket.inet_aton(get_ip_address())
+    
+    info = ServiceInfo("_http._tcp.local.",
+                       "Team14RaspberryRoomReservation._http._tcp.local.",
+                       localip, 8081, 0, 0, desc
+                       )
+    
+    zeroconf = Zeroconf()
+    zeroconf.register_service(info)
+    print("Zeroconf service registered.")
+        
+except:
+    pass
+#------------------------------------------------------------
+
 #Mongo DB Connection
 connection = MongoClient('localhost', 27017)
 db = connection.reservations
@@ -23,15 +76,19 @@ def eventfunc():
     from_time = json_obj['fromTime']
     to_time = json_obj['toTime']
     room = json_obj['room']
+	
+	#--new--
+	gcal_id = json_obj['gcal_id']
+	#-------
 
     if method == "POST":
         return newevent(phone_num, first, last, day, from_time, to_time, room)
     elif method == "PUT":
         id = json_obj['_id']['$oid']
         print ('ObjectId: ', ObjectId(id))
-        return editevent(id, phone_num, first, last, day, from_time, to_time, room)
+        return editevent(id, phone_num, first, last, day, from_time, to_time, room, gcal_id)
     elif method == "DELETE":
-        return deleteevent(phone_num, first, last, day, from_time, to_time, room)
+        return deleteevent(phone_num, first, last, day, from_time, to_time, room, gcal_id)
 
 @app.route('/reservations/<string:phone_num>', methods=['GET'])
 def phonefunc(phone_num):
@@ -102,16 +159,7 @@ def newevent(phone_num, first, last, day, from_time, to_time, room):
         else:
 <<<<<<< HEAD
             print('no conflict!')
-            entry_id = db.reservations.insert({'phone_number': phone_num, 'first': first, 'last': last, 'date': day, 'fromTime': from_time, 'toTime': to_time, 'room': room })
-            return toJSON(db.reservations.find({'_id': ObjectId(entry_id)}))
-    error_obj = {'error': 'time conflict found'}
-    return json.dumps(error_obj, indent=2)
-    
-=======
-            print('you\'re good')
-            db.reservations.insert({'phone_number': phone_num, 'first': first, 'last': last, 'date': day, 'fromTime': from_time, 'toTime': to_time, 'room': room })
-			
-            #---------------------Create the event in Google Calendar-------------------------
+	    #---------------------Create the event in Google Calendar-------------------------
             credentials = get_credentials()
             http = credentials.authorize(httplib2.Http())
             service = discovery.build('calendar', 'v3', http=http)
@@ -133,15 +181,25 @@ def newevent(phone_num, first, last, day, from_time, to_time, room):
               },
             }
             event = service.events().insert(calendarId='primary', body=event).execute()
-            print ('Event created: %s' % (event.get('htmlLink')))
+            #print ('Event created! Here is your unique Event ID: %s' % (event.get('id')))
+	    calendarid = event.get('id')
+	    print ('You may confirm your event here: %s' % (event.get('htmlLink')))
 	    #----------------------------------------------------------------------------------
+	    
+            entry_id = db.reservations.insert({'phone_number': phone_num, 'first': first, 'last': last, 'date': day, 'fromTime': from_time, 'toTime': to_time, 'room': room , 'gcal_id' = calendarid})
+            print('you\'re good')
+            return toJSON(db.reservations.find({'_id': ObjectId(entry_id)}))
+    error_obj = {'error': 'time conflict found'}
+    return json.dumps(error_obj, indent=2)
+    
+=======		
 			
 >>>>>>> 761aed37b07f70fff2aac6a1716480bfeccae969
 def userevents(phone_num):
     results = db.reservations.find({'phone_number':phone_num})
     return toJSON(results)
 
-def editevent(id, phone_num, first, last, day, from_time, to_time, room):
+def editevent(id, phone_num, first, last, day, from_time, to_time, room, gcal_id):
     results = db.reservations.find({'date':day, 'room':room})
     json_results = []
     for result in results:
@@ -169,14 +227,43 @@ def editevent(id, phone_num, first, last, day, from_time, to_time, room):
         else:
             print('you\'re good')
             db.reservations.update_one({'_id': ObjectId(id)}, {'$set':{'phone_number': phone_num, 'first': first, 'last': last, 'date': day, 'fromTime': from_time, 'toTime': to_time, 'room': room }})
+				
+	    #---------------------Edit the event in Google Calendar-------------------------
+            credentials = get_credentials()
+            http = credentials.authorize(httplib2.Http())
+            service = discovery.build('calendar', 'v3', http=http)
+			
+            event = service.events().get(calendarId='primary', eventId=id).execute()
+			
+            event['summary'] = 'Raspberry Room Reservation'
+            event['location'] = room
+            event['description'] = ("Reservation for {}, created by {} {}.").format(room,first,last)
+            event['start'] = {
+                    'dateTime': ("{}T{}").format(day,from_time),
+                    'timeZone': 'EST',
+            }
+	    event['end'] = {
+                    'dateTime': ("{}T{}").format(day,to_time),
+                    'timeZone': 'EST',
+            }
+	    event['attendees'] = {
+                    'phone': phone_num,
+            }
+			
+	    updated_event = service.events().update(calendarId='primary', eventId=gcal_id, body=event).execute()
+			
+	    print("Your event has been updated!")
+            print('Please feel free to confirm your changes here: %s' % (event.get('htmlLink')))
+	    #----------------------------------------------------------------------------------
             return toJSON(db.reservations.find({'_id': ObjectId(id)}))
     
     error_obj = {'error': 'time conflict found'}
     return json.dumps(error_obj, indent=2)
 
-def deleteevent(phone_num, first, last, day, from_time, to_time, room):
-    result = db.reservations.delete_one({'phone_number': phone_num, 'first': first, 'last': last, 'date': day, 'fromTime': from_time, 'toTime': to_time, 'room': room })
+def deleteevent(phone_num, first, last, day, from_time, to_time, room, gcal_id):
+    result = db.reservations.delete_one({'phone_number': phone_num, 'first': first, 'last': last, 'date': day, 'fromTime': from_time, 'toTime': to_time, 'room': room, 'gcal_id': gcal_id})
     delete_obj = {'deleted count': result.deleted_count}
+    service.events().delete(calendarId='primary', eventId=gcal_id).execute()
     return json.dumps(delete_obj, indent=2)
     
 def dailysearch(day):
